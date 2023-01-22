d8d5c9965c48e96fbc119b17192ea04539d00ece
habibor-rahaman1010 <habibor144369@gmail.com>
2023-01-22 12:24:18 +0600

    ok done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace MainProgram
+﻿using System.Collections.Generic;
+namespace MainProgram
 {
     class Program
     {
         static void Main(string[] args)
         {
+            List<string> name = new List<string>() { "Bill gates", "Microsoft" };
+            name.Add("Habibor Rahaman");
+            name.Add("Tamim Sahriar Subeen");
+            name.Add("Tamid Rafi");
+            name.Add("Tamid Rafi");
+            name.Add("Tamid Rafi");
 
+            for (int i = 0; i < name.Count; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
         }
     }
 }
